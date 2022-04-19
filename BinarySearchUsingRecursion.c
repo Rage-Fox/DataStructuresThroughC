e381aea8491012102de918568f740163e8b3dc52
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-04-19 11:26:00 +0530

    Add files via upload

---
+++ BinarySearchUsingRecursion.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int binarysearch(int *arr,int e,int l,int r)
+{
+	int m;
+	if(l>r)
+	{
+		return 0;
+	}
+	else
+	{
+		m=(l+r)/2;
+		if(arr[m]==e)
+		{
+			return 1;
+		}
+		if(arr[m]<e)
+		{
+			l=m+1;
+		}
+		if(arr[m]>e)
+		{
+			r=m-1;
+		}
+	}
+	return binarysearch(arr,e,l,r);
+}
+int main()
+{
+	int n,e,i,j,k,t;
+	scanf("%d",&n);
+	int l=0,r=n-1,arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				t=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=t;
+			}
+		}
+	}
+	scanf("%d",&e);
+	if(binarysearch(arr,e,l,r))
+	{
+		printf("Element found");
+	}
+	else
+	{
+		printf("Element not found");
+	}
+}
